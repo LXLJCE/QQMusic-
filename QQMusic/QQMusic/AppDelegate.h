aaa34a66b923d71559b90df649ecf084b7c9e8e7
刘嘉欣 <liujiaxin@liujiaxindeMacBook-Pro.local>
2015-09-11 23:58:42 +0800

    QQ主要播放器

---
+++ QQMusic/QQMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QQMusic
+//
+//  Created by 刘嘉欣 on 15/9/11.
+//  Copyright (c) 2015年 刘嘉欣. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
